d21631004ad5aa8f4b4e5133a767e429bd5e1bba
tyort <neotyo1986@gmail.com>
2026-06-27 00:12:25 +0300

    фон

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import './Header.css';
 
 export function Header() {
@@ -6,6 +7,12 @@ export function Header() {
       <div className="app-header-logo">
         <img src="/images/logo.png" alt="Логотип Антипрофи" className="app-header-logo-img" />
       </div>
+      <nav className="app-header-nav">
+        <a href="#order" className="app-header-nav-link">
+          <ShoppingCartIcon fontSize="small" />
+          <span>Сделать заказ</span>
+        </a>
+      </nav>
       <h1 className="app-title">Антипрофи</h1>
       <p className="app-slogan">Мы предлагаем услуги, за которые нам немного стыдно</p>
     </header>
