1530eba090b7de6300c5bceb4ca4f1a7abfe7759
tyort <neotyo1986@gmail.com>
2026-06-27 16:30:36 +0300

    Подключаем шрифты

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import localFont from 'next/font/local'
 import { Header } from '../components/Header/Header'
 import { Footer } from '../components/Footer/Footer'
 import '../index.css'
@@ -8,13 +9,19 @@ export const metadata = {
   description: 'Antiprofi Store',
 }
 
+const nunito = localFont({
+  src: '../fonts/ofont.ru_Nunito.woff2',
+  variable: '--font-nunito',
+  display: 'swap',
+})
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="ru">
+    <html lang="ru" className={nunito.variable}>
       <body>
         <Header />
         <main className="main-content">
