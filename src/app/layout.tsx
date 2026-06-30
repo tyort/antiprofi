15294771dd72884d1042cabbe5db8a8366e47b09
tyort <neotyo1986@gmail.com>
2026-06-30 19:39:06 +0300

    favicon, eslint, seo, performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,15 @@ export const metadata = {
   metadataBase: new URL('https://anti-profi.ru'),
   title: 'Antiprofi',
   description: 'Antiprofi Store',
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.ico', sizes: 'any' },
+    ],
+    apple: '/apple-touch-icon.png',
+    shortcut: '/favicon.ico',
+  },
   openGraph: {
     title: 'Antiprofi',
     description: 'Antiprofi Store',
