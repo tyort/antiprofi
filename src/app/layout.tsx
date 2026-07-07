2e4c4b4b97f9b48df9487f0f2d6ecb89b33edf22
tyort <neotyo1986@gmail.com>
2026-07-07 18:43:53 +0300

    Add Yandex Metrika and Google Analytics tracking scripts to layout
    - Integrated Yandex Metrika for enhanced analytics tracking based on environment variable.
    - Added Google Analytics tracking script with configuration for improved user insights.
    - Ensured scripts load after interactive phase for better performance.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import localFont from 'next/font/local'
+import Script from 'next/script'
 import { Header } from '../components/Header/Header'
 import { Footer } from '../components/Footer/Footer'
 import '../index.css'
@@ -47,6 +48,45 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
+        {process.env.NEXT_PUBLIC_YANDEX_METRIKA_ID && (
+          <>
+            <Script id="yandex-metrika" strategy="afterInteractive" dangerouslySetInnerHTML={{
+              __html: `
+                (function(m,e,t,r,i,k,a){
+                    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                    m[i].l=1*new Date();
+                    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+                })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=${process.env.NEXT_PUBLIC_YANDEX_METRIKA_ID}', 'ym');
+
+                ym(${process.env.NEXT_PUBLIC_YANDEX_METRIKA_ID}, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+              `
+            }} />
+            <noscript>
+              <div>
+                <img src={`https://mc.yandex.ru/watch/${process.env.NEXT_PUBLIC_YANDEX_METRIKA_ID}`} style={{ position: 'absolute', left: '-9999px' }} alt="" />
+              </div>
+            </noscript>
+          </>
+        )}
+        
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive" dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+              `
+            }} />
+          </>
+        )}
       </body>
     </html>
   )
